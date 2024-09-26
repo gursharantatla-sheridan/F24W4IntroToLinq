fa61e0609e0f68706ad87267e3a653d669d814bf
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-26 14:16:03 -0400

    Add project files.

---
+++ F24W4IntroToLinq/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace F24W4IntroToLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 4, 3, 5, 6, 7, 4, 2, 3, 5, 6, 3, 5 };
+
+            var greaterThan4 = from n in array
+                               where n > 4
+                               orderby n
+                               select n;
+
+            foreach (var i in greaterThan4)
+                Console.Write(i + " ");
+            Console.WriteLine("\n\n");
+
+
+
+        }
+    }
+}
